c95504d9059c7838107fe3d9848e90cb95536adc
David Neumann <davidjneumann@gmail.com>
2019-03-11 01:23:24 -0400

    Add scaffold for game capture code

---
+++ src/Infrastructure/WFGameCapture/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WFGameCapture
+{
+    public class GameCapture
+    {
+        public string GetTradeChatImage()
+        {
+            throw new NotImplementedException();
+            //Should return the path to where it saved the capture.
+        }
+    }
+}
