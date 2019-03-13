23220354c32d39e5a308259d90f6b37d6ad74d49
David Neumann <davidjneumann@gmail.com>
2019-03-13 03:58:04 -0400

    Add a new approach to generate OCR maps

--- src/Infrastructure/WFImageParser/ChatImageCleaner.cs
+++ src/Infrastructure/WFImageParser/ChatImageCleaner.cs
@@ -8,12 +8,17 @@ using SixLabors.Primitives;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WFImageParser
 {
     public class ChatImageCleaner : IChatImageProcessor
     {
+        public ChatImageCleaner(CharInfo[] knownCharacters)
+        {
+            _knownCharacters = knownCharacters;
+        }
         /// <summary>
         /// Converts the full color game window into a image of the chat window in grayscale.
         /// </summary>
@@ -23,7 +28,7 @@ namespace WFImageParser
         public string ProcessChatImage(string imagePath, string outputDirectory)
         {
             var converter = new ColorSpaceConverter();
-            var chatRect = new Rectangle(5, 750, 3249, 1350);
+            var chatRect = new Rectangle(4, 893, 3236, 1350);
             //Image.Load(@"C:\Users\david\OneDrive\Documents\WFChatParser\Screenshot (7).png")
             //@"C:\Users\david\OneDrive\Documents\WFChatParser\friday_last_moved.png"
             using (Image<Rgba32> rgbImage = Image.Load(imagePath))
@@ -32,18 +37,18 @@ namespace WFImageParser
                     chatRect = new Rectangle(4, 530, 2022, 850);
                 else if (rgbImage.Height == 1080)
                     chatRect = new Rectangle(4, 370, 1507, 650);
-                
+
                 rgbImage.Mutate(x => x.Crop(chatRect));
 
-                var maxHsv = 0.29;
-                
+                var minV = 0.29;
+
                 for (int i = 0; i < rgbImage.Width * rgbImage.Height; i++)
                 {
                     var x = i % rgbImage.Width;
                     var y = i / rgbImage.Width;
                     var pixel = rgbImage[x, y];
                     var hsvPixel = converter.ToHsv(pixel);
-                    if (hsvPixel.V > maxHsv)
+                    if (hsvPixel.V > minV)
                     {
                         rgbImage[x, y] = Rgba32.Black;
                     }
@@ -55,6 +60,241 @@ namespace WFImageParser
                 var outputPath = Path.Combine(outputDirectory, file.Name);
                 rgbImage.Save(outputPath);
                 return outputPath;
+            }
+        }
+
+        private int[] _lineOffsets = new int[] { 5, 55, 105, 154, 204, 253, 303, 352, 402, 452, 501, 551, 600, 650, 700, 749, 799, 848, 898, 948, 997, 1047, 1096, 1146, 1195, 1245, 1295 };
+        private readonly CharInfo[] _knownCharacters;
+
+
+        public void SaveGreyscaleImage(string imagePath, string outputPath, float minV = 0.29f)
+        {
+            var converter = new ColorSpaceConverter();
+
+            using (Image<Rgba32> rgbImage = Image.Load(imagePath))
+            {
+                for (int i = 0; i < rgbImage.Width * rgbImage.Height; i++)
+                {
+                    var x = i % rgbImage.Width;
+                    var y = i / rgbImage.Width;
+                    var pixel = rgbImage[x, y];
+                    var hsvPixel = converter.ToHsv(pixel);
+                    if (hsvPixel.V > minV)
+                    {
+                        rgbImage[x, y] = Rgba32.Black;
+                    }
+                    else
+                        rgbImage[x, y] = Rgba32.White;
+                }
+
+                rgbImage.Save(outputPath);
+            }
+        }
+        public CharInfo[] AnalyzeInput(string imagePath, string referenceChars, float minV)
+        {
+            var converter = new ColorSpaceConverter();
+            using (Image<Rgba32> rgbImage = Image.Load(imagePath))
+            {
+                //var maxHsv = 0.29;
+                var hitPoints = new int[36];
+                var hitPointCount = 0;
+                var onChar = false;
+                var charInfos = new List<CharInfo>();
+                CharInfo currentChar = null;
+                for (int x = 0; x < rgbImage.Width; x++)
+                {
+                    hitPointCount = 0;
+                    for (int y = 0; y < rgbImage.Height; y++)
+                    {
+                        var pixel = rgbImage[x, y];
+                        var hsvPixel = converter.ToHsv(pixel);
+                        if (hsvPixel.V > minV)
+                        {
+                            hitPoints[hitPointCount++] = y;
+                        }
+                    }
+
+                    if (hitPointCount > 0)
+                    {
+                        if (!onChar)
+                        {
+                            onChar = true;
+                            currentChar = new CharInfo();
+                            if (charInfos.Count == referenceChars.Length)
+                                return new CharInfo[0];
+                            currentChar.Character = referenceChars[charInfos.Count];
+                        }
+                        var cleanPoints = new int[hitPointCount];
+                        for (int i = 0; i < hitPointCount; i++)
+                        {
+                            cleanPoints[i] = hitPoints[i];
+                        }
+                        currentChar.Slices.Add(new List<int>(cleanPoints));
+                        currentChar.Width++;
+                    }
+                    if (onChar && hitPointCount == 0)
+                    {
+                        charInfos.Add(currentChar);
+                        currentChar = null;
+                        onChar = false;
+                    }
+                }
+
+                return charInfos.ToArray();
+            }
+        }
+
+        public string VerifyInput(string imagePath, float minV)
+        {
+            var converter = new ColorSpaceConverter();
+            using (Image<Rgba32> rgbImage = Image.Load(imagePath))
+            {
+                var hitPoints = new int[36];
+                var hitPointCount = 0;
+                var chars = new System.Text.StringBuilder();
+                IEnumerable<CharInfo> possibleChars = null;
+                var sliceIndex = 0;
+                for (int x = 0; x < rgbImage.Width; x++)
+                {
+                    hitPointCount = 0;
+                    for (int y = 0; y < rgbImage.Height; y++)
+                    {
+                        var pixel = rgbImage[x, y];
+                        var hsvPixel = converter.ToHsv(pixel);
+                        if (hsvPixel.V > minV)
+                        {
+                            hitPoints[hitPointCount++] = y;
+                        }
+                    }
+
+                    if (hitPointCount > 0)
+                    {
+                        IEnumerable<CharInfo> newPossibleChars = null;
+                        if (possibleChars == null)
+                            newPossibleChars = _knownCharacters.Where(c => DoesCharactersMatch(0, hitPoints, hitPointCount, c)).ToList();
+                        else
+                            newPossibleChars = possibleChars.Where(c => DoesCharactersMatch(sliceIndex, hitPoints, hitPointCount, c)).ToList();
+
+                        if (newPossibleChars.Count() > 1)
+                        {
+                            possibleChars = newPossibleChars;
+                            sliceIndex++;
+                        }
+                        else if (newPossibleChars.Count() == 1)
+                        {
+                            sliceIndex = 0;
+                            var character = newPossibleChars.Single();
+                            chars.Append(character.Character);
+                            x += character.Width - 1;
+                        }
+                    }
+                    else
+                        possibleChars = null;
+                }
+                return chars.ToString();
+            }
+        }
+
+        private static bool DoesCharactersMatch(int sliceIndex, int[] hitPoints, int hitPointCount, CharInfo character)
+        {
+            if (character.Slices.Count <= sliceIndex || character.Slices[sliceIndex].Count != hitPointCount)
+                return false;
+
+            var valid = true;
+            for (int i = 0; i < hitPointCount; i++)
+            {
+                if (character.Slices[sliceIndex][i] != hitPoints[i])
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            return valid;
+        }
+
+        public string AnalysisChatMessage(string imagePath, string outputDirectory)
+        {
+            var converter = new ColorSpaceConverter();
+            var chatRect = new Rectangle(4, 763, 3249, 1337);
+            //Image.Load(@"C:\Users\david\OneDrive\Documents\WFChatParser\Screenshot (7).png")
+            //@"C:\Users\david\OneDrive\Documents\WFChatParser\friday_last_moved.png"
+            using (Image<Rgba32> rgbImage = Image.Load(imagePath))
+            {
+                if (rgbImage.Height == 1440)
+                    chatRect = new Rectangle(4, 530, 2022, 850);
+                else if (rgbImage.Height == 1080)
+                    chatRect = new Rectangle(4, 370, 1507, 650);
+
+                rgbImage.Mutate(x => x.Crop(chatRect));
+
+                var maxHsv = 0.29;
+                var hitPoints = new int[36];
+                var hitPointCount = 0;
+                for (int line = 0; line < _lineOffsets.Length; line++)
+                {
+                    for (int x = chatRect.Left; x < chatRect.Right; x++)
+                    {
+                        hitPointCount = 0;
+                        for (int y = _lineOffsets[line] + chatRect.Top; y < _lineOffsets[line] + chatRect.Top; y++)
+                        {
+                            var pixel = rgbImage[x, y];
+                            var hsvPixel = converter.ToHsv(pixel);
+                            if (hsvPixel.V > maxHsv)
+                            {
+                                hitPoints[hitPointCount++] = y;
+                            }
+                        }
+                        if (hitPointCount > 0)
+                        {
+                            //Check database of known characters
+                        }
+                    }
+                }
+
+                //for (int i = 0; i < rgbImage.Width * rgbImage.Height; i++)
+                //{
+                //    var x = i % rgbImage.Width;
+                //    var y = i / rgbImage.Width;
+                //    var pixel = rgbImage[x, y];
+                //    var hsvPixel = converter.ToHsv(pixel);
+                //    if (hsvPixel.V > maxHsv)
+                //    {
+                //        rgbImage[x, y] = Rgba32.Black;
+                //    }
+                //    else
+                //        rgbImage[x, y] = Rgba32.White;
+                //}
+                //var sb = new System.Text.StringBuilder();
+                //var fileInfo = new FileInfo(imagePath);
+                //sb.AppendLine(fileInfo.Name);
+                //var line = 0;
+                //var prevLineBlack = false;
+                //var lineOffsets = new int[27];
+                //for (int y = 0; y < rgbImage.Height; y++)
+                //{
+                //    var x = 2;
+                //    var pixel = rgbImage[x, y];
+                //    var hsvPixel = converter.ToHsv(pixel);
+                //    if (hsvPixel.V > maxHsv && !prevLineBlack)
+                //    {
+                //        rgbImage[x, y] = Rgba32.Black;
+                //        lineOffsets[line] = y - 3;
+                //        sb.AppendLine("Line " + ++line + " [ at y: " + y);
+                //        prevLineBlack = true;
+                //    }
+                //    else if(hsvPixel.V <= maxHsv)
+                //        prevLineBlack = false;
+                //}
+
+                //Console.Write("private int[] _lineOffsets = new int[]{");
+                //foreach (var offset in lineOffsets)
+                //{
+                //    Console.Write(offset + ",");
+                //}
+
+                //return sb.ToString();
+                throw new NotImplementedException();
             }
         }
     }
--- src/Presentation/DebugCLI/Program.cs
+++ src/Presentation/DebugCLI/Program.cs
@@ -1,10 +1,12 @@
 ﻿using ImageOCR;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using WFGameCapture;
 using WFImageParser;
 
@@ -12,26 +14,57 @@ namespace DebugCLI
 {
     class Program
     {        
-        static string outputDir = @"C:\Users\david\OneDrive\Documents\WFChatParser\Test Runs\Run 19\Outputs";
+        static string outputDir = @"C:\Users\david\OneDrive\Documents\WFChatParser\Test Runs\Run 23\Outputs";
 
         static void Main(string[] args)
         {
             if (!Directory.Exists(outputDir))
                 Directory.CreateDirectory(outputDir);
 
-            //Console.WriteLine("Push enter and then switch to warframe");
-            //Console.ReadLine();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"\rStarting in {5 - i} seconds...");
-            //    System.Threading.Thread.Sleep(1000);
-            //}
+            //MonitorChatLive();
 
-            //var capture = new GameCapture();
-            //capture.GetTradeChatImage();
-
-            ProcessChatLogs();
+            AnalyseImages();
+            //ProcessChatLogs();
             //ProcessRivens();
+        }
+
+        private static void MonitorChatLive()
+        {
+            Console.WriteLine("Push enter and then switch to warframe");
+            Console.ReadLine();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"\rStarting in {5 - i} seconds...");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            var capture = new GameCapture();
+            var p = new ChatImageCleaner(JsonConvert.DeserializeObject<CharInfo[]>("chars.json"));
+            var t = new ImageParser();
+
+            using (var fout = new System.IO.StreamWriter("output.txt"))
+            {
+                string[] messageHistory = new string[100];
+                var index = 0;
+                while (true)
+                {
+                    var image = capture.GetTradeChatImage();
+                    var processedImagePath = p.ProcessChatImage(image, Environment.CurrentDirectory);
+                    var text = t.ParseChatImage(processedImagePath);
+                    foreach (var line in text.ChatTextLines)
+                    {
+                        if (!messageHistory.Contains(line))
+                        {
+                            Console.WriteLine(line);
+                            fout.WriteLine(line);
+                            fout.Flush();
+                            messageHistory[index++] = line;
+                            if (index >= messageHistory.Length)
+                                index = 0;
+                        }
+                    }
+                }
+            }
         }
 
         private static void ProcessRivens()
@@ -60,7 +93,7 @@ namespace DebugCLI
         {
             var sw = new Stopwatch();
             sw.Start();
-            var p = new ChatImageCleaner();
+            var p = new ChatImageCleaner(JsonConvert.DeserializeObject<CharInfo[]>("chars.json"));
             var t = new ImageParser();
             sw.Stop();
             Console.WriteLine("Initialize finished in: " + sw.Elapsed.TotalSeconds + "s");
@@ -94,6 +127,45 @@ namespace DebugCLI
 
             var averageTime = fileTimes.Aggregate(0d, (acc, i) => acc + i) / fileTimes.Count;
             Console.WriteLine($"Average screenshot processing time: {averageTime}s");
+            Console.WriteLine("Jobs done");
+        }
+        
+        private static void AnalyseImages()
+        {
+            var p = new ChatImageCleaner(JsonConvert.DeserializeObject<CharInfo[]>(File.ReadAllText("chars.json")));
+            Console.WriteLine("=Processing files=");
+            foreach (var file in Directory.GetFiles(@"C:\Users\david\OneDrive\Documents\WFChatParser\Test Runs\Text Inputs"))
+            {
+                var name = file.Substring(file.LastIndexOf('\\') + 1);
+                Console.WriteLine($"=={name}==");
+
+                var outputFile = Path.Combine(outputDir, name);
+
+                var chars = "! # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \\ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~ – — ’ ‚".Replace(" ", "");
+                var rand = new Random();
+                for (int i = 0; i < 27; i++)
+                {
+                    var sb = new StringBuilder();
+                    foreach (var character in chars.OrderBy(x => rand.Next()))
+                    {
+                        sb.Append(character + " ");
+                    }
+                    Console.WriteLine(sb.ToString().Trim() + " [" + "\n");
+                }
+                var validVs = new List<float>();
+                for (float v = 1.0f; v >= 0.29; v -= 0.01f)
+                {
+                    p.SaveGreyscaleImage(file, outputFile, v);
+                    var charInfoResults = p.AnalyzeInput(file, chars, v);
+                    var newP = new ChatImageCleaner(charInfoResults);
+                    var charResults = newP.VerifyInput(file, v);
+                    if (charResults.SequenceEqual(chars))
+                        validVs.Add(v);                        
+                }
+
+                //var processedImagePath = p.AnalysisChatMessage(file, outputDir);
+                //Console.WriteLine(processedImagePath);
+            }
             Console.WriteLine("Jobs done");
         }
     }
