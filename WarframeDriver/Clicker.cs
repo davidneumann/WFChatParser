3f1f6112c27a9b82efd813ec98c746514a9aa828
David Neumann <davidjneumann@gmail.com>
2019-03-20 03:34:38 -0400

    Add simple mouse movement helper

---
+++ WarframeDriver/Clicker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace WarframeDriver
+{
+    public class Clicker
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, UIntPtr dwExtraInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+
+        public void MoveCursorTo(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+        public void ClickAt(uint x, uint y)
+        {
+            POINT p;
+            GetCursorPos(out p);
+            if (p.X != x || p.Y != y)
+                MoveCursorTo((int)x, (int)y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, (UIntPtr)0);
+        }
+    }
+
+    /// <summary>
+    /// Struct representing a point.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public static implicit operator Point(POINT point)
+        {
+            return new Point(point.X, point.Y);
+        }
+    }
+}
