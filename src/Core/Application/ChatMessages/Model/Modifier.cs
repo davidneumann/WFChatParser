cebbc847e96a8da2dfc9ba4bc115b855fe20ebaa
David Neumann <davidjneumann@gmail.com>
2019-05-28 17:59:55 -0400

    Update projectile speed modifier string to new value

--- src/Core/Application/ChatMessages/Model/Modifier.cs
+++ src/Core/Application/ChatMessages/Model/Modifier.cs
@@ -10,7 +10,7 @@ namespace Application.ChatMessages.Model
         public bool Curse { get; set; }
 
         private static Regex _valRegx = new Regex(@"([-+]?[\d\.]+)");
-        private static string[] _possibleDescriptions = new string[] { "% Zoom", "% Weapon Recoil", "% Toxin", "% Status Duration", "% Status Chance", "% Critical Hit Chance for Slide Attack", "% Slash", "s Combo Duration", "% Reload Speed", "% Range", "% Puncture", "Punch Through", "% Projectile Flight Speed", "% Multishot", "% Melee Damage", "% Magazine Capacity", "% Impact", "% Heat", "% Fire Rate(x2 for Bows)", "% Fire Rate", "% Finisher Damage", "% Electricity", "% Damage to Infested", "% Damage to Grineer", "% Damage to Corpus", "% Damage", "% Critical Damage", "% Critical Chance", "% Cold", "% Channeling Efficiency", "% Channeling Damage", "% Attack Speed", "% Ammo Maximum" };
+        private static string[] _possibleDescriptions = new string[] { "% Zoom", "% Weapon Recoil", "% Toxin", "% Status Duration", "% Status Chance", "% Critical Hit Chance for Slide Attack", "% Slash", "s Combo Duration", "% Reload Speed", "% Range", "% Puncture", "Punch Through", "% Projectile Speed", "% Multishot", "% Melee Damage", "% Magazine Capacity", "% Impact", "% Heat", "% Fire Rate(x2 for Bows)", "% Fire Rate", "% Finisher Damage", "% Electricity", "% Damage to Infested", "% Damage to Grineer", "% Damage to Corpus", "% Damage", "% Critical Damage", "% Critical Chance", "% Cold", "% Channeling Efficiency", "% Channeling Damage", "% Attack Speed", "% Ammo Maximum" };
 
         public static Modifier ParseString(string modifier)
         {
