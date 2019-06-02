23be65a9a988bed3670742e9b46a0e2dfd5b8b57
Justin Harper <gigapatches@gmail.com>
2019-06-02 13:50:17 -0400

    Speed up HSV calculations
    This also caches them into a lookup table

--- src/Core/Application/Utils/ColorExtensions.cs
+++ src/Core/Application/Utils/ColorExtensions.cs
@@ -5,26 +5,65 @@ using System.Text;
 
 namespace Application.Utils
 {
-    public class Hsv
+    public struct Hsv
     {
         public float Hue { get; set; }
         public float Saturation { get; set; }
         public float Value { get; set; }
     }
+
     public static class ColorExtensions
     {
-        //Credit Greg - (https://stackoverflow.com/a/1626175)
+        private static readonly Hsv[,,] LookupTable = new Hsv[256, 256, 256];
+
+        static ColorExtensions()
+        {
+            for (var r = 0; r <= 255; r++)
+            {
+                for (var g = 0; g <= 255; g++)
+                {
+                    for (var b = 0; b <= 255; b++)
+                    {
+                        LookupTable[r, g, b] = Color.FromArgb(255, r, g, b).ToHsvReal();
+                    }
+                }
+            }
+        }
+
+        // Credit sam - (http://lolengine.net/blog/2013/01/13/fast-rgb-to-hsv)
+        public static Hsv ToHsvReal(this Color color)
+        {
+            var hsv = new Hsv();
+
+            var r = color.R / 255f;
+            var g = color.G / 255f;
+            var b = color.B / 255f;
+
+            var k = 0f;
+
+            if (g < b)
+            {
+                (g, b) = (b, g);
+                k = -1f;
+            }
+
+            if (r < g)
+            {
+                (g, r) = (r, g);
+                k = -2 / 6f - k;
+            }
+
+            var chroma = r - Math.Min(g, b);
+            hsv.Hue = Math.Abs(k + (g - b) / (6 * chroma + 1e-20f)) * 360;
+            hsv.Saturation = chroma / (r + 1e-20f);
+            hsv.Value = r;
+
+            return hsv;
+        }
+
         public static Hsv ToHsv(this Color color)
         {
-            int max = Math.Max(color.R, Math.Max(color.G, color.B));
-            int min = Math.Min(color.R, Math.Min(color.G, color.B));
-
-            return new Hsv()
-            {
-                Hue = color.GetHue(),
-                Saturation = (max == 0) ? 0 : 1f - (1f * min / max),
-                Value = max / 255f
-            };
+            return LookupTable[color.R, color.G, color.B];
         }
     }
 }
