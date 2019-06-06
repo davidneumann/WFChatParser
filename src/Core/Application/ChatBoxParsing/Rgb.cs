c76383e782ee837ad656bc1a66510d6b05e5b7be
David Neumann <davidjneumann@gmail.com>
2019-06-06 19:24:22 -0400

    Add helpers to come up with a average pixel per line

---
+++ src/Core/Application/ChatBoxParsing/Rgb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.ChatBoxParsing
+{
+    public struct Rgb
+    {
+        byte R;
+        byte G;
+        byte B;
+
+        public Rgb(byte red, byte green, byte blue)
+        {
+            R = red;
+            G = green;
+            B = blue;
+        }
+    }
+}
