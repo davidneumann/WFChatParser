c387ea326558555b6edcbec973e024d4022e010e
David Neumann <davidjneumann@gmail.com>
2019-11-10 03:24:29 -0500

    Add helpers for parsing chinese riven modis

--- src/Core/Application/ChatMessages/Model/Modifier.cs
+++ src/Core/Application/ChatMessages/Model/Modifier.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Enums;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,20 +16,27 @@ namespace Application.ChatMessages.Model
         public bool Curse { get; set; }
 
         private static Regex _valRegx = new Regex(@"([-+]?[\d\.]+)");
-        private static string[] _possibleDescriptions = new string[] { "% Puncture", "% Melee Combo Efficiency", "Initial Combo", "% Toxin", "% Damage to Corpus", "% Critical Damage", "% Channeling Damage", "% Cold", "% Magazine Capacity", "% Damage", "% Reload Speed", "% Damage to Infested", "% Slash", "% Status Chance", "% Ammo Maximum", "% Multishot", "% Projectile Speed", "Punch Through", "% Weapon Recoil", "% Damage to Grineer", "% Impact", "% Status Duration", "% Fire Rate(x2 for Bows)", "% Critical Chance for Slide Attack", "% Fire Rate", "% Critical Chance", "% Zoom", "% Electricity", "Range", "% Finisher Damage", "% Melee Damage", "% Heat", "% Attack Speed", "s Combo Duration", "% Channeling Efficiency", "% Chance to gain extra Combo Count" };
-
+        //private static string[] _possibleEnglishDescriptions = new string[] { "% Puncture", "% Melee Combo Efficiency", "Initial Combo", "% Toxin", "% Damage to Corpus", "% Critical Damage", "% Channeling Damage", "% Cold", "% Magazine Capacity", "% Damage", "% Reload Speed", "% Damage to Infested", "% Slash", "% Status Chance", "% Ammo Maximum", "% Multishot", "% Projectile Speed", "Punch Through", "% Weapon Recoil", "% Damage to Grineer", "% Impact", "% Status Duration", "% Fire Rate(x2 for Bows)", "% Critical Chance for Slide Attack", "% Fire Rate", "% Critical Chance", "% Zoom", "% Electricity", "Range", "% Finisher Damage", "% Melee Damage", "% Heat", "% Attack Speed", "s Combo Duration", "% Channeling Efficiency", "% Chance to gain extra Combo Count" };
+        private static Dictionary<string, string[]> _possibleDescriptions = new Dictionary<string, string[]>();
+        public static IReadOnlyDictionary<string, string[]> PossibleDescriptions { get { return (IReadOnlyDictionary<string, string[]>)_possibleDescriptions; } }
         private static bool _hasCheckedForUpdates = false;
             
-        public Modifier()
+        static Modifier()
         {
-            if(!_hasCheckedForUpdates)
+            _possibleDescriptions["en"] = new string[] { "% Puncture", "% Melee Combo Efficiency", "Initial Combo", "% Toxin", "% Damage to Corpus", "% Critical Damage", "% Channeling Damage", "% Cold", "% Magazine Capacity", "% Damage", "% Reload Speed", "% Damage to Infested", "% Slash", "% Status Chance", "% Ammo Maximum", "% Multishot", "% Projectile Speed", "Punch Through", "% Weapon Recoil", "% Damage to Grineer", "% Impact", "% Status Duration", "% Fire Rate(x2 for Bows)", "% Critical Chance for Slide Attack", "% Fire Rate", "% Critical Chance", "% Zoom", "% Electricity", "Range", "% Finisher Damage", "% Melee Damage", "% Heat", "% Attack Speed", "s Combo Duration", "% Channeling Efficiency", "% Chance to gain extra Combo Count" };
+            _possibleDescriptions["zh"] = new string[] { "% 近战伤害", "% 穿刺伤害", "% 冲击伤害", "% 切割伤害", "% 暴击几率", "% 暴击伤害", "% 电击伤害", "% 火焰伤害", "% 冰冻伤害", "% 毒素伤害", "% 触发时间", "% 对 Corpus 伤害", "% 对 Grineer 伤害", "% 对 Infested 伤害", "% 攻击速度", "% 触发几率", "秒连击持续时间", "% 导引伤害", "% 导引效率", "滑行攻击 % 暴击几率。", "攻击范围", "% 处决伤害", "% 射速（弓类武器效果加倍）", "% 弹药最大值", "% 弹匣容量", "% 伤害", "% 多重射击", "% 投射物速度", "穿透", "% 武器后坐力", "% 装填速度", "% 变焦", "% 射速", "% 近战连击效率", "初始连击", "% 不获得连击几率", "% 额外连击数几率", };
+
+            if (!_hasCheckedForUpdates)
             {
                 _hasCheckedForUpdates = true;
                 using (var client = new WebClient())
                 {
                     try
                     {
-                        _possibleDescriptions = JsonConvert.DeserializeObject<string[]>(client.DownloadString("https://10o.io/rivens/buffnames.json"));
+                        //_possibleDescriptions["en"] = JsonConvert.DeserializeObject<string[]>(client.DownloadString("https://10o.io/rivens/buffnames.json"));
+                        var fullJson = JObject.Parse(client.DownloadString("https://10o.io/rivens/rivendata.json"));
+                        _possibleDescriptions["en"] = fullJson["buffs"].Cast<KeyValuePair<string, JToken>>().Select(p => (string)p.Value["text"]["en"]).Select(str => str.Replace("|val|", "").Trim()).Distinct().ToArray();
+                        _possibleDescriptions["zh"] = fullJson["buffs"].Cast<KeyValuePair<string, JToken>>().Select(p => (string)p.Value["text"]["zh"]).Select(str => str.Replace("|val|", "").Trim()).Distinct().ToArray();
                     }
                     catch
                     {
@@ -46,9 +55,15 @@ namespace Application.ChatMessages.Model
             var roughDesc = _valRegx.Replace(modifier, "").TrimEnd();
             var description = roughDesc;
             if (clientLanguage == ClientLanguage.English)
-                description = _possibleDescriptions.OrderBy(desc => Utils.LevenshteinDistance.Compute(roughDesc, desc)).First();
+                description = _possibleDescriptions["en"].OrderBy(desc => Utils.LevenshteinDistance.Compute(roughDesc, desc)).First();
             else if (clientLanguage == ClientLanguage.Chinese)
-                description = roughDesc.Replace(" ", "");
+            {
+                roughDesc = roughDesc.Trim();
+                if (roughDesc.StartsWith("% "))
+                    description = $"% {roughDesc.Substring(2).Replace(" ", "").Replace("(", "（").Replace(")", "）").Trim()}";
+                else
+                    description = roughDesc.Replace(" ", "").Replace("(", "（").Replace(")", "）").Trim();
+            }
             result.Description = description;
             return result;
         }
@@ -56,11 +71,6 @@ namespace Application.ChatMessages.Model
         public override string ToString()
         {
             return (Value > 0 ? "+": "") + Value + Description;
-        }
-
-        public static void OverrideDescriptions(string[] newDescriptions)
-        {
-            _possibleDescriptions = newDescriptions;
         }
     }
 }
