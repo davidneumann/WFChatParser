3baf07d8937c9ca9bf30d2cfca0f2adf6885b36d
David Neumann <davidjneumann@gmail.com>
2019-11-12 14:14:29 -0500

    Refactor HsvImage into RivenImage

--- src/Infrastructure/ImageOCR/ComplexRivenParser.cs
+++ src/Infrastructure/ImageOCR/ComplexRivenParser.cs
@@ -12,8 +12,7 @@ namespace ImageOCRBad
     public class ComplexRivenParser
     {
         private ClientLanguage _clientLanguage;
-        private static Hsv _minPurple = Hsv.FromHsv(270f * 0.99f, 0.385f * 0.99f, 0.835f * 0.99f);
-        private static Hsv _maxPurple = Hsv.FromHsv(270f * 1.01f, 0.385f * 1.01f, 0.835f * 1.01f);
+        const int _bodyBottomY = 548;
 
         public ComplexRivenParser(ClientLanguage clientLanguage)
         {
@@ -22,9 +21,9 @@ namespace ImageOCRBad
 
         public void DebugGetLineDetails(Bitmap b)
         {
-            GetLineDetails(new HsvImage(b));
+            GetLineDetails(new RivenImage(b));
         }
-        private List<LineDetails> GetLineDetails(HsvImage croppedRiven)
+        private List<LineDetails> GetLineDetails(RivenImage croppedRiven)
         {
             if (croppedRiven.Width != 466)
                 throw new Exception("Riven not cropped");
@@ -57,26 +56,34 @@ namespace ImageOCRBad
                     croppedRiven[x, y] = Hsv.Black;
                 }
             }
+
+            //Warm up the cache
+            croppedRiven.CacheRect(new Rectangle(376, 0, 87, 42)); //Drain/polairty
+            croppedRiven.CacheRect(new Rectangle(51, 565, 362, 34)); //MR/rerolls
+            var leftBackgroundRect = new Rectangle(croppedRiven.Width / 3 - 7, 46, 15, _bodyBottomY - 46); //Left scan line for background
+            var rightBackgroundRect = new Rectangle((croppedRiven.Width / 3) * 2 - 7, 46, 15, _bodyBottomY - 46); //Right scan line for background
+            croppedRiven.CacheRect(leftBackgroundRect);
+            croppedRiven.CacheRect(rightBackgroundRect);
+
             var results = new List<LineDetails>();
             var pastBackground = false;
             var startY = 0;
-            var leftStartOffset = (croppedRiven.Width / 2) - (croppedRiven.Width / 3);
-            var rightStartOffset = (croppedRiven.Width / 2) + (croppedRiven.Width / 3);
-            for (int y = 46; y < 548; y++)
+            for (int y = 46; y < _bodyBottomY; y++)
             {
                 if (!pastBackground)
                 {
                     var Vs = new float[30];
                     for (int x = 0; x < 15; x++)
                     {
-                        Vs[x] = croppedRiven.GetFastVertical(leftStartOffset + x, y).Value;
-                        Vs[x + 15] = croppedRiven.GetFastVertical(rightStartOffset + x, y).Value;
+                        Vs[x] = croppedRiven[x + leftBackgroundRect.Left, y].Value;
+                        Vs[x + 15] = croppedRiven[x + rightBackgroundRect.Left, y].Value;
                     }
                     if (Vs.Average() >= 0.165)
                         continue;
                     else
                     {
                         pastBackground = true;
+                        croppedRiven.CacheRect(new Rectangle(8, y, 450, _bodyBottomY - y));//Rest of the text
                     }
                 }
                 if (pastBackground)//Not else as we need the first occurance
@@ -93,32 +100,17 @@ namespace ImageOCRBad
             //DEBUG
             using (var debugBitmap = new Bitmap(croppedRiven.Width, croppedRiven.Height))
             {
-                var highlights = new bool[croppedRiven.Width, croppedRiven.Height];
                 for (int x = 0; x < croppedRiven.Width; x++)
                 {
                     for (int y = 0; y < croppedRiven.Height; y++)
                     {
-                        if (IsPurple(croppedRiven[x, y]))
-                            highlights[x, y] = true;
-                        //debugBitmap.SetPixel(x, y, Color.Black);
-                        else
-                            highlights[x, y] = false;
-                        //debugBitmap.SetPixel(x, y, Color.White);
-                    }
-                }
-                for (int x = 1; x < croppedRiven.Width - 1; x++)
-                {
-                    for (int y = 1; y < croppedRiven.Height - 1; y++)
-                    {
-                        if (!highlights[x, y] && HasNeighbor(highlights, x, y))
+                        if (croppedRiven.IsPurple(x, y) || croppedRiven.HasNeighbor(x, y))
                         {
                             //croppedRiven.Restore(x, y);
                             var p = croppedRiven[x, y];
                             var v = byte.MaxValue - (byte)(byte.MaxValue * Math.Min(1f, Math.Max(0f, p.Value - 0.153f) / (0.835f - 0.153f)));
                             debugBitmap.SetPixel(x, y, Color.FromArgb(v, v, v));
                         }
-                        else if (highlights[x, y])
-                            debugBitmap.SetPixel(x, y, Color.Black);
                         else
                             debugBitmap.SetPixel(x, y, Color.White);
                     }
@@ -135,66 +127,95 @@ namespace ImageOCRBad
             return results;
         }
 
-        private bool HasNeighbor(bool[,] highlights, int x, int y)
-        {
-            var width = highlights.GetLength(0);
-            var height = highlights.GetLength(1);
-            const int distance = 3;
-            for (int x2 = x - distance; x2 < x + distance; x2++)
-            {
-                if (x2 < 0 || x2 >= width)
-                    continue;
-                for (int y2 = y - distance; y2 < y + distance; y2++)
-                {
-                    if (y2 < 0 || y2 >= height)
-                        continue;
-                    if (highlights[x2, y2])
-                        return true;
-                }
-            }
-            return false;
-            //return highlights[x - 1, y - 1].Value < 0.5f //Top
-            //    || highlights[x + 1, y].Value < 0.5f //Right
-            //    || highlights[x, y + 1].Value < 0.5f //Bottom
-            //    || highlights[x - 1, y].Value < 0.5f; //Left
-        }
-
-        private bool IsPurple(Hsv hsv)
-        {
-            return hsv.Hue >= _minPurple.Hue && hsv.Hue <= _maxPurple.Hue
-                && hsv.Saturation >= _minPurple.Saturation && hsv.Saturation <= _maxPurple.Saturation
-                && hsv.Value >= _minPurple.Value && hsv.Saturation <= _maxPurple.Value;
-            //return hsv.Hue >= _minPurple.Hue && hsv.Hue <= _maxPurple.Hue;
-        }
-
-        private class HsvImage
+        private class RivenImage
         {
             private Hsv[,] _hsvs;
-            private bool[,] _cache;
+            private bool[,] _purples;
+            private bool[,] _purplesCache;
+            private bool[,] _hsvCache;
             private Bitmap _image;
-            public HsvImage(Bitmap image)
+            public RivenImage(Bitmap image)
             {
                 _hsvs = new Hsv[image.Width, image.Height];
-                _cache = new bool[image.Width, image.Height];
+                _hsvCache = new bool[image.Width, image.Height];
+                _purples = new bool[image.Width, image.Height];
+                _purplesCache = new bool[image.Width, image.Height];
                 _image = image;
+            }
+            private static Hsv _minPurple = Hsv.FromHsv(270f * 0.99f, 0.385f * 0.99f, 0.835f * 0.99f);
+            private static Hsv _maxPurple = Hsv.FromHsv(270f * 1.01f, 0.385f * 1.01f, 0.835f * 1.01f);
+
+            public static bool IsPurple(Hsv hsv)
+            {
+                return hsv.Hue >= _minPurple.Hue && hsv.Hue <= _maxPurple.Hue
+                    && hsv.Saturation >= _minPurple.Saturation && hsv.Saturation <= _maxPurple.Saturation
+                    && hsv.Value >= _minPurple.Value && hsv.Saturation <= _maxPurple.Value;
+                //return hsv.Hue >= _minPurple.Hue && hsv.Hue <= _maxPurple.Hue;
+            }
+            public bool IsPurple(int x, int y)
+            {
+                if (!_purplesCache[x, y])
+                    _purples[x, y] = IsPurple(this[x, y]);
+                return _purples[x, y];
             }
 
             public Hsv this[int x, int y]
             {
                 get
                 {
-                    if (!_cache[x, y])
+                    if (!_hsvCache[x, y])
                     {
                         _hsvs[x, y] = _image.GetPixel(x, y).ToHsv();
-                        _cache[x, y] = true;
+                        _hsvCache[x, y] = true;
                     }
                     return _hsvs[x, y];
                 }
                 set
                 {
-                    _cache[x, y] = true;
+                    _hsvCache[x, y] = true;
                     _hsvs[x, y] = value;
                 }
+            }
+
+            public void CacheRect(Rectangle rect)
+            {
+                for (int x = rect.Left; x < rect.Right; x++)
+                {
+                    for (int y = rect.Top; y < rect.Height; y++)
+                    {
+                        if (_hsvCache[x, y])
+                            continue;
+                        else
+                        {
+                            _hsvCache[x, y] = true;
+                            _hsvs[x, y] = _image.GetPixel(x, y).ToHsv();
+                            _purples[x, y] = IsPurple(_hsvs[x, y]);
+                            _purplesCache[x, y] = true;
+                        }
+                    }
+                }
+            }
+
+            public bool HasNeighbor(int x, int y)
+            {
+                const int distance = 3;
+                for (int x2 = x - distance; x2 < x + distance; x2++)
+                {
+                    if (x2 < 0 || x2 >= Width)
+                        continue;
+                    for (int y2 = y - distance; y2 < y + distance; y2++)
+                    {
+                        if (y2 < 0 || y2 >= Height)
+                            continue;
+                        if (IsPurple(x2, y2))
+                            return true;
+                    }
+                }
+                return false;
+                //return highlights[x - 1, y - 1].Value < 0.5f //Top
+                //    || highlights[x + 1, y].Value < 0.5f //Right
+                //    || highlights[x, y + 1].Value < 0.5f //Bottom
+                //    || highlights[x - 1, y].Value < 0.5f; //Left
             }
 
             public int Width
@@ -204,43 +225,6 @@ namespace ImageOCRBad
             public int Height
             {
                 get { return _image.Height; }
-            }
-
-            public void Restore(int x, int y)
-            {
-                _cache[x, y] = true;
-                _hsvs[x, y] = _image.GetPixel(x, y).ToHsv();
-            }
-
-            public Hsv GetFastHorizontal(int x, int y)
-            {
-                if (!_cache[x, y])
-                {
-                    //var data = _image.LockBits(new Rectangle(x, 0, 1, Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, _image.PixelFormat);
-                    //TODO: somehow store these pixels into _hsv
-                    for (int y2 = 0; y2 < _image.Height; y2++)
-                    {
-                        _hsvs[x, y2] = _image.GetPixel(x, y2).ToHsv();
-                        _cache[x, y2] = true;
-                    }
-                }
-
-                return _hsvs[x, y];
-            }
-
-            public Hsv GetFastVertical(int x, int y)
-            {
-                if (!_cache[x, y])
-                {
-                    //var data = _image.LockBits(new Rectangle(0, y, Width, 1), System.Drawing.Imaging.ImageLockMode.ReadOnly, _image.PixelFormat);
-                    //TODO: somehow store these pixels into _hsv
-                    for (int y2 = 0; y2 < _image.Height; y2++)
-                    {
-                        _hsvs[x, y2] = _image.GetPixel(x, y2).ToHsv();
-                        _cache[x, y2] = true;
-                    }
-                }
-                return _hsvs[x, y];
             }
         }
         private class LineDetails
