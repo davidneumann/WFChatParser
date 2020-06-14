f2aa4aed65eddb764b66bc39d3328c008875cc20
David Neumann <davidjneumann@gmail.com>
2020-06-14 17:26:40 -0400

    Use Semlar's text encoding fixe in gap fixer

--- src/Presentation/DebugCLI/fixgaps.py
+++ src/Presentation/DebugCLI/fixgaps.py
@@ -1,7 +1,19 @@
 import json
+import codecs
+
+def bom_open(path, *args, **kwargs):
+    with open(path, 'rb') as f:
+        raw = f.read(4)
+    for enc, boms in \
+            ('utf-8-sig', (codecs.BOM_UTF8,)), \
+            ('utf-32', (codecs.BOM_UTF32_LE, codecs.BOM_UTF32_BE)), \
+            ('utf-16', (codecs.BOM_UTF16_LE, codecs.BOM_UTF16_BE)):
+        if any(raw.startswith(bom) for bom in boms):
+            return open(path, *args, **kwargs, encoding=enc)
+    return open(path, *args, **kwargs)
 
 ligatures = []
-with open("gapcombos.txt", "r") as f:
+with bom_open("gapcombos.txt", "r") as f:
 	for line in f.read().split("\n"):
 		if "," in line:
 			(left, right) = line.split(",")
@@ -16,6 +28,7 @@ with open("gaps.json", "rb") as f:
 	lefts = []
 	rights = []
 	for ligature in ligatures:
+		# print(ligature)
 		for gap in gaps:
 			if gap.get("Right") == ligature["left"]:
 				rights.append({
