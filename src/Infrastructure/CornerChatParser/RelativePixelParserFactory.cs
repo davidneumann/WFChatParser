a994c2ec6577fdfe6b84df04b303964ae54eb565
David Neumann <davidjneumann@gmail.com>
2020-07-16 21:32:32 -0400

    Add a RelativePixelParserFactory

---
+++ src/Infrastructure/CornerChatParser/RelativePixelParserFactory.cs
@@ -0,0 +1,16 @@
+﻿using Application.Enums;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelativeChatParser
+{
+    public class RelativePixelParserFactory : IChatParserFactory
+    {
+        public IChatParser CreateChatParser(ClientLanguage clientLanguage)
+        {
+            return new RelativePixelParser();
+        }
+    }
+}
