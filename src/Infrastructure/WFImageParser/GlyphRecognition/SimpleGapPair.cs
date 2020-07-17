3dd7c65e6e0d73c2bb1bafd937866789652e0e46
David Neumann <davidjneumann@gmail.com>
2020-07-16 23:58:00 -0400

    Remove unused projects from the repo

--- src/Infrastructure/WFImageParser/GlyphRecognition/SimpleGapPair.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WFImageParser.GlyphRecognition
-{
-    internal class SimpleGapPair
-    {
-        public string Left { get; set; }
-        public string Right { get; set; }
-        public int Gap { get; set; }
-    }
-}
