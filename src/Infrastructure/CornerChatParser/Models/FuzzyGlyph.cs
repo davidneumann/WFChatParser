2746c0e800024a41f3a15e0edc83ccabce8ee255
David Neumann <davidjneumann@gmail.com>
2020-07-22 18:23:24 -0400

    Add .Clone to FuzzyGlyph

--- src/Infrastructure/CornerChatParser/Models/FuzzyGlyph.cs
+++ src/Infrastructure/CornerChatParser/Models/FuzzyGlyph.cs
@@ -10,24 +10,51 @@ namespace RelativeChatParser.Models
     [DebuggerDisplay("{Character}")]
     public class FuzzyGlyph
     {
-        public string Character;
-        public Point3[] RelativePixelLocations;
-        [JsonIgnore] public Point3[] RelativeBrights;
-        public Point[] RelativeEmptyLocations;
-        public float AspectRatio;
-        public int ReferenceMaxWidth;
-        public int ReferenceMaxHeight;
-        public int ReferenceMinWidth;
-        public int ReferenceMinHeight;
-        public float ReferenceGapFromLineTop;
-        public bool IsOverlap = false;
-        public Point[] RelativeCombinedLocations;
+        public string Character { get; set;}
+        public Point3[] RelativePixelLocations { get; set;}
+        [JsonIgnore] public Point3[] RelativeBrights { get; set;}
+        public Point[] RelativeEmptyLocations { get; set;}
+        public float AspectRatio { get; set;}
+        public int ReferenceMaxWidth { get; set;}
+        public int ReferenceMaxHeight { get; set;}
+        public int ReferenceMinWidth { get; set;}
+        public int ReferenceMinHeight { get; set;}
+        public float ReferenceGapFromLineTop { get; set;}
+        public bool IsOverlap { get; set;} = false;
+        public Point[] RelativeCombinedLocations { get; set;}
 
+        //public FuzzyGlyph(string character, Point3[] relativePixelLocations, Point[] relativeEmptyLocations, float aspectRatio, int referenceMaxWidth, int referenceMaxHeight, int referenceMinWidth, int referenceMinHeight, 
+        //    float referenceGapFromLineTop, bool isOverlap)
+        //{
+        //    this.Character = character;
+        //    this.RelativePixelLocations = relativePixelLocations;
+        //    this.RelativeEmptyLocations = relativeEmptyLocations;
+        //    this.AspectRatio = aspectRatio;
+        //    this.ReferenceMaxWidth = referenceMaxWidth;
+        //    this.ReferenceMaxHeight = referenceMaxHeight;
+        //    this.ReferenceMinWidth = referenceMinWidth;
+        //    this.ReferenceMinHeight = referenceMinHeight;
+        //    this.ReferenceGapFromLineTop = referenceGapFromLineTop;
+        //    this.IsOverlap = isOverlap;
+        //}
         public FuzzyGlyph()
         {
+
+        }
+
+        public FuzzyGlyph Clone()
+        {
+            var clone = (FuzzyGlyph)this.MemberwiseClone();
+            clone.Character = string.Copy(Character);
+            clone.RelativePixelLocations = (Point3[]) RelativePixelLocations.Clone();
+            clone.RelativeBrights = (Point3[])RelativeBrights.Clone();
+            clone.RelativeEmptyLocations = (Point[])RelativeEmptyLocations.Clone();
+            clone.RelativeCombinedLocations = (Point[])RelativeCombinedLocations.Clone();
+
+            return clone;
         }
     }
-    
+
     public class Glyph
     {
         public string Character { get; set; }
